330bdd1ec5821dc9830f621c1d431ee7c463bb7e
ratul41907 <arafatzaman419@gmail.com>
2024-05-18 21:44:21 +0600

    First Commit

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -14,7 +14,8 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TOD code application logic here
+        System.out.println("Hello");
     }
     
 }
